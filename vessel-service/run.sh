af8b3a5b3835cd230787772d4b20f4f857ae3300
root <root@proxy.c.pso-suchit.internal>
2018-08-02 21:34:53 +0000

    working example with nginx

--- vessel-service/run.sh
+++ vessel-service/run.sh
@@ -1,4 +1,6 @@
 echo 'starting http server on port 9000'
 ./http2_server >> go.logs & 
+echo 'starting health check server on port 8000'
+python -m SimpleHTTPServer 8000 >> /dev/null & 
 echo 'starting gRPC server on port 443'
 python server.py
